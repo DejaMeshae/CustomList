c348f5696ba81d85df485735d3dd88ff3fa42be2
DejaMeshae <daemeshae@yahoo.com>
2019-04-09 10:31:20 -0500

    Created unittest and referenced it to Week5CustomList projec $

---
+++ CustomListUnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CustomListUnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CustomListUnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3e751868-1cc8-474d-adb6-1335ad1c9ab9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CustomListUnitTestProject/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Week5CustomList;
+namespace CustomListUnitTestProject
+{
+    [TestClass]
+    public class CustomUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            GenericList genericList = new GenericList();
+        }
+    }
+}
